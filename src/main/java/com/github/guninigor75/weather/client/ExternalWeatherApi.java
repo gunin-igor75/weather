fddca44da9623ce4f400645a6a33d017fd119bce
Igor Gunin <gunin-igor75@yandex.ru>
2023-10-04 16:40:48 +0600

    add circuitbreaker

--- src/main/java/com/github/guninigor75/weather/client/ExternalWeatherApi.java
+++ src/main/java/com/github/guninigor75/weather/client/ExternalWeatherApi.java
@@ -1,6 +1,8 @@
 package com.github.guninigor75.weather.client;
 
 import com.github.guninigor75.weather.client.model.WeatherApiResponse;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 public interface ExternalWeatherApi {
 
     @GetMapping
+    @CircuitBreaker(name = "weather-breaker")
+    @Retry(name = "weather-retry")
     WeatherApiResponse getWeather(
             @RequestParam(name = "lon") double longitude,
             @RequestParam(name = "lat") double latitude);
